93a0c27595e7b39c9c16700783b35b765a400a13
Michael Kevin <michaelkmb@hotmail.com>
2022-02-18 20:48:54 -0300

    + urls by recursive include;

--- project/project/urls.py
+++ project/project/urls.py
@@ -14,15 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from recipes.views import about, contact, home
+from django.urls import include, path
 
 urlpatterns = [
-    path('', home),
-    
-    path('sobre/', about),
-    path('contato/', contact),
-    #path('', ),
+    #clients pages
+    path('', include('recipes.urls')),
 
+    #admins pages
     path('admin/', admin.site.urls),
 ]
---
+++ project/recipes/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from recipes.views import about, contact, home
+
+urlpatterns = [
+    path('', home),
+    
+    path('sobre/', about),
+    path('contato/', contact),
+    #path('', ),
+]
