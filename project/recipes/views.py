37d7482ac10efa0aa7e553d677463ac37503500a
Michael Kevin <michaelkmb@hotmail.com>
2022-02-18 21:00:11 -0300

    + templates html for views;

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('\r\nHello World')
+    #return HttpResponse('\r\nHello World')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('\r\nHello About')
+    return render(request, 'about.html')
 
 def contact(request):
-    return HttpResponse('\r\nHello Contact')
+    return render(request, 'contact.html')
